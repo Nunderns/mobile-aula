fc23c0def51c441b8785167898d188d3e0776304
Henri Okayama <henri.okayama@gmail.com>
2025-03-25 22:11:41 -0300

    Criar uma model de Contatos

---
+++ app/src/main/java/com/example/uatizapinatoracme/modelos/Contato.java
@@ -0,0 +1,49 @@
+package com.example.uatizapinatoracme.modelos;
+
+import java.util.Objects;
+
+public class Contato {
+    private int contatoId;
+    private String contato;
+    private String celular;
+    private String email;
+
+    public Contato(int contatoId, String contato, String celular, String email) {
+        this.contatoId = contatoId;
+        this.contato = contato;
+        this.celular = celular;
+        this.email = email;
+    }
+
+    public int getContatoId() {
+        return contatoId;
+    }
+
+    public void setContatoId(int contatoId) {
+        this.contatoId = contatoId;
+    }
+
+    public String getContato() {
+        return contato;
+    }
+
+    public void setContato(String contato) {
+        this.contato = contato;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
