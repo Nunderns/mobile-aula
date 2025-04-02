b8c54f0a2588516cca06371ea8efcb8c4f56a7c5
Henri Okayama <henri.okayama@gmail.com>
2025-04-01 21:24:12 -0300

    Implementação do banco de dados

--- app/src/main/java/com/example/uatizapinatoracme/bd/DBHelper.java
+++ app/src/main/java/com/example/uatizapinatoracme/bd/DBHelper.java
@@ -1,21 +1,31 @@
 package com.example.uatizapinatoracme.bd;
 
+import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DBHelper extends SQLiteOpenHelper {
 
+    private static final String DB_NAME = "contatos.db";
+    private static final int DB_VERSION = 1;
+
     public DBHelper(Context context){
-        super(context, )
+        super(context, DB_NAME, null, DB_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        String sql = "CREATE TABLE contatos (" +
+                "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "nome TEXT NOT NULL, " +
+                "celular TEXT NOT NULL, " +
+                "email TEXT)";
+        db.execSQL(sql);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL("DROP TABLE IF EXISTS contatos");
+        onCreate(db);
     }
 }
