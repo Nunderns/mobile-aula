cb2e4f68f002d2f4143c5cd43b5ba0a0f761575c
Henri Okayama <henri.okayama@gmail.com>
2025-04-01 21:17:46 -0300

    Adição de cores

---
+++ app/src/main/java/com/example/uatizapinatoracme/bd/DBHelper.java
@@ -0,0 +1,21 @@
+package com.example.uatizapinatoracme.bd;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    public DBHelper(Context context){
+        super(context, )
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
